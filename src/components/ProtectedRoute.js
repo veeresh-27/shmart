4231ad1e6a3eb37883e444280105484fc0c0b3d9
Veeresh R M <veereshrm789@gmail.com>
2022-06-19 01:10:34 +0530

    hostings

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,10 +1,17 @@
+
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/UserAuthContext";
 
 export default function ProtectedRoute({children}){
-    let {user} = useUserAuth();
-    if(!user){
-       return <Navigate to='/'/>
+    let {user} =  useUserAuth();
+    localStorage.setItem('user',JSON.stringify(user));
+    let User = async () => {
+        let user = await JSON.parse(localStorage.getItem('user'));
+        return user;
     }
-    return children;
+    let user1 = User();
+    if(user1.email){ 
+       return <Navigate to='/'/>
+    }else
+        return children;
 }
